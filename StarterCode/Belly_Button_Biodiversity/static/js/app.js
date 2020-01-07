895a10c45fe788216e7e72c4a061eb6e1246c04a
VanessaKnox <knox.vanessa.a@gmail.com>
2020-01-07 14:43:33 -0500

    commit

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,13 +4,19 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
+    d3.json(`/metadata/${sample}`).then(function (data) {
+      var Metadatasample = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    Metadatasample.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    Object.entries(data).forEach(([key, value]) => {
+      var li = Metadatasample.append("li").text(`${key}:${value}`);
+    });
+  });
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 }
@@ -18,13 +24,49 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`)
+  .then(
+    function (response) {
 
-    // @TODO: Build a Bubble Chart using the sample data
+      var bubbleChartData = [{
+        x: response.otu_ids,
+        y: response.sample_values,
+        mode: "markers",
+        type: "scatter",
+        marker: {
+          size: response.sample_values,
+          color: response.otu_ids
+        }
+      }];
+
+      var bubbleChartLayout = {
+        xaxis: { title: "OUT ID" },
+        showlegend: false
+      };
+
+      Plotly.newPlot("bubble", bubbleChartData, bubbleChartLayout);
+   
+      // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
 }
+  var pieChartData = [{
+          values: response.sample_values.slice(1, 10),
+          labels: response.otu_ids.slice(0, 10),
+          type: "pie"
+        }];
+
+        var pieChartLayout = {
+          height: 600,
+          width: 800
+        };
+
+        Plotly.newPlot("pie", pieChartData, pieChartLayout);
+      }
+    )
+};
 
 function init() {
   // Grab a reference to the dropdown select element
